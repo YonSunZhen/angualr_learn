e7359e0b12cc25ac4794d6015f4090859415ec05
YonSunZhen <1315976441@qq.com>
2020-04-05 21:24:20 +0800

    0405

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { from } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-learn';
+
+
+  ngOnInit() {
+    // 如何监听 title 值的变化
+    const Observable = from(this.title);
+    Observable.subscribe((x) => {
+      console.log(x);
+    });
+    Observable.subscribe((x) => {
+      console.log(x);
+    });
+  }
+
+  onClick() {
+    this.title = this.title === 'angular-learn' ? '666' : 'angular-learn';
+  }
 }
